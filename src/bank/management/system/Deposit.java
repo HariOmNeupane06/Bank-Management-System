4b0b8db7a9dcd3ef1784c7947d0b500a37248fad
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-09-19 22:29:04 +0545

    Add deposit functionality

--- src/bank/management/system/Deposit.java
+++ src/bank/management/system/Deposit.java
@@ -1,4 +1,46 @@
 package bank.management.system;
 
-public class deposit {
+import javax.swing.*;
+import java.awt.*;
+
+public class Deposit extends JFrame {
+    String pin;
+    TextField textField;
+    JButton b1, b2;
+
+
+    Deposit(String pin){
+        this.pin = pin;
+        ImageIcon i1 =  new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830, Image.SCALE_DEFAULT);
+        ImageIcon i3 =  new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+        JLabel label1 = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("System",Font.BOLD,16));
+        label1.setBounds(460,180,400,35);
+        l3.add(label1);   // we use l3. because to show the text label on the image not the frame.
+
+        textField = new TextField();
+        textField.setBackground(new Color(65,125,128));
+        textField.setForeground(Color.WHITE);
+        textField.setBounds(460,230,320,25);
+        textField.setFont(new Font("Raleway",Font.BOLD,22));
+        l3.add(textField);
+
+        b1 =  new JButton("DEPOSIT");
+
+
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Deposit("");
+    }
 }
